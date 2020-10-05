e2e765a5b2ffe744ddc1292f50a2e9458f640d1d
peter <peter.reijnders@verpeteren.nl>
2020-10-05 09:23:35 +0200

    ignore: outline of structure

--- src/plan.rs
+++ src/plan.rs
@@ -2,6 +2,8 @@ use crate::batchneed::BatchNeed;
 use crate::factory::Factory;
 use crate::volume::Volume;
 
+use z3::{Config, Context, SatResult, Solver};
+
 pub struct Plan {
     dummy: u8,
 }
@@ -9,6 +11,44 @@ pub struct Plan {
 impl Plan {
     pub fn new() -> Self {
         Self { dummy: 4 }
+    }
+    pub fn do_magic(&self) {
+        let mut config = Config::new();
+        config.set_proof_generation(false);
+        config.set_model_generation(true);
+        config.set_debug_ref_count(false);
+        config.set_timeout_msec(5_000);
+        let context = Context::new(&config);
+        let solver = Solver::new(&context);
+        // for batch in batches
+        //   for step in batch
+        //      set start first step in future
+        //      set end of step .. or .. after start
+        //      set start of transfer operation after end of step
+        //      set end of  transfer .. after start of transfer
+        //      set start of of next step ... after end of transfer
+        //      set start of clean operation after end of transfer
+        //      set end of clean operation .. after start of clean
+        // avoid duplicate use of machine during operation, transfer or cleaning
+        // set transfer and clean operation only during office hours
+        // set transfer and clean operation nod during holidays
+        //todo bottleneck first
+        match solver.check() {
+            SatResult::Sat => {
+                let model = solver.get_model();
+                //let used = model.eval(z3var).unwrap().as_bool().unwrap();
+            }
+            SatResult::Unsat => {
+                // println!("No solution found!");
+            }
+            SatResult::Unknown => {
+                print!(
+                    "No solution found: {}",
+                    solver.get_reason_unknown().unwrap()
+                );
+                panic!("TODO: better error handling");
+            }
+        }
     }
 }
 
